ce030e809b72a364c5cde659eb2977ec42c22f61
DavyMerlo <davymerlo@live.be>
2023-11-11 21:07:50 +0100

    fixed some minor bugs

--- src/main/java/com/davy/restapi/authetication/controller/AuthenticationController.java
+++ src/main/java/com/davy/restapi/authetication/controller/AuthenticationController.java
@@ -16,25 +16,25 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/api/v1/auth/")
 @RequiredArgsConstructor
 public class AuthenticationController {
 
     private final AuthenticationServiceImp service;
 
-    @PostMapping("/register")
+    @PostMapping("register")
     public ResponseEntity<AuthenticationResponse> register(
             @RequestBody RegisterRequest request){
         return ResponseEntity.ok(service.register(request));
     }
 
-    @PostMapping("/authenticate")
+    @PostMapping("authenticate")
     public ResponseEntity<AuthenticationResponse> register(
             @RequestBody AuthenticationRequest request){
         return ResponseEntity.ok(service.authenticate(request));
     }
 
-    @PostMapping("/refresh-token")
+    @PostMapping("refresh-token")
     public void refreshToken(
             HttpServletRequest request,
             HttpServletResponse response
--- src/main/java/com/davy/restapi/authetication/request/RegisterRequest.java
+++ src/main/java/com/davy/restapi/authetication/request/RegisterRequest.java
@@ -13,27 +13,27 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class RegisterRequest {
 
-    @NotEmpty(message = "password should not be empty")
-    @NotNull(message = "password should not be empty")
-    private String firstname;
+//    @NotEmpty(message = "password should not be empty")
+//    @NotNull(message = "password should not be empty")
+    private String firstName;
+
+
+//    @NotEmpty(message = "password should not be empty")
+//    @NotNull(message = "password should not be empty")
+    private String lastName;
 
 
     @NotEmpty(message = "password should not be empty")
     @NotNull(message = "password should not be empty")
-    private String lastname;
-
-
-    @NotEmpty(message = "password should not be empty")
-    @NotNull(message = "password should not be empty")
-    @Email(regexp = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}",
-            flags = Pattern.Flag.CASE_INSENSITIVE,
-            message = "Email-address is not valid")
+//    @Email(regexp = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}",
+//            flags = Pattern.Flag.CASE_INSENSITIVE,
+//            message = "Email-address is not valid")
     private String email;
 
 
     @NotEmpty(message = "Password should not be empty")
     @NotNull(message = "Password should not be empty")
-    @Size(min=8, message = "Password must be at least 8 characters long")
+//    @Size(min=8, message = "Password must be at least 8 characters long")
     private String password;
 
 
@@ -44,33 +44,33 @@ public class RegisterRequest {
 
     @NotEmpty(message = "Street should not be empty")
     @NotNull(message = "Street should not be empty")
-    @Pattern(regexp="^[A-Za-z]*$",message = "Street should contains only letters")
-    @Size(min = 1, max = 35, message = "Street should be between 1 and 35 letters long")
+//    @Pattern(regexp="^[A-Za-z]*$",message = "Street should contains only letters")
+//    @Size(min = 1, max = 35, message = "Street should be between 1 and 35 letters long")
     private String street;
 
 
     @NotEmpty(message = "Housenumber should not be empty")
     @NotNull(message = "housenumber should not be empty")
-    @Pattern(regexp = "^[0-9]+(\\.[0-9]+)?$", message = "Housenumber should be a numeric value")
-    @Size(min = 1, max = 5, message = "Housenumber should be between 1 and 35 letters long")
+//    @Pattern(regexp = "^[0-9]+(\\.[0-9]+)?$", message = "Housenumber should be a numeric value")
+//    @Size(min = 1, max = 5, message = "Housenumber should be between 1 and 35 letters long")
     private String houseNumber;
 
 
-    @Pattern(regexp = "^[0-9]+(\\.[0-9]+)?$", message = "Busnumber should be a numeric value")
-    @Size(min = 1, max = 5)
+//    @Pattern(regexp = "^[0-9]+(\\.[0-9]+)?$", message = "Busnumber should be a numeric value")
+//    @Size(min = 1, max = 5)
     private String busNumber;
 
 
     @NotEmpty(message = "Postalcode should not be empty")
     @NotNull(message = "Postalcode should not be empty")
-    @Pattern(regexp = "^[0-9]+(\\.[0-9]+)?$", message = "Postalcode should be a numeric value")
-    @Size(min = 1, max = 4, message = "Postalcode should be between 1 and 4 digits long")
+//    @Pattern(regexp = "^[0-9]+(\\.[0-9]+)?$", message = "Postalcode should be a numeric value")
+//    @Size(min = 1, max = 4, message = "Postalcode should be between 1 and 4 digits long")
     private String postalCode;
 
 
     @NotEmpty(message = "Localauthority should not be empty")
     @NotNull(message = "Localauthority should not be empty")
-    @Pattern(regexp="^[A-Za-z]*$",message = "Localauthority should contains only letters")
-    @Size(min = 1, max = 35, message = "Localauthority should be between 1 and 35 letters long")
+//    @Pattern(regexp="^[A-Za-z]*$",message = "Localauthority should contains only letters")
+//    @Size(min = 1, max = 35, message = "Localauthority should be between 1 and 35 letters long")
     private String localAuthority;
 }
