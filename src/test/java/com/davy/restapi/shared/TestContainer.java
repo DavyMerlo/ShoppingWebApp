29756a43b40868a6f33373ae7029a91e1ebeeafc
DavyMerlo <davymerlo@live.be>
2024-02-27 00:02:15 +0100

    Created a testsuite

--- src/test/java/com/davy/restapi/shared/TestContainer.java
+++ src/test/java/com/davy/restapi/shared/TestContainer.java
@@ -3,18 +3,26 @@ package com.davy.restapi.shared;
 import org.junit.jupiter.api.*;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class TestContainer {
+
+    public static TestRestTemplate restTemplate;
+
+    @LocalServerPort
+    public int port = 8880;
 
     @ServiceConnection
     @Container
@@ -31,15 +39,10 @@ public class TestContainer {
         registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
     }
 
-    @BeforeEach
-    void shouldConnectionWithTestContainer() {
-        Assertions.assertTrue(postgreSQLContainer.isRunning());
-    }
-
-    @AfterAll
-    static void tearDown() {
-        if (postgreSQLContainer != null && postgreSQLContainer.isRunning()) {
-            postgreSQLContainer.stop();
-        }
+    @BeforeAll
+    public static void setUp() {
+        restTemplate = new TestRestTemplate();
     }
 }
+
+
